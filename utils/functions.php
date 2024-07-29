b06861915595a1c9a0cb3336ac833f15476bf57b
mss-souloftware <sufyan.souloftware@gmail.com>
2024-07-29 18:36:50 +0500

    changes

--- utils/functions.php
+++ utils/functions.php
@@ -1,11 +1,11 @@
 <?php
-// Prevent direct access
+
 if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
+    exit;
 }
 
 // Example of a general function
 function booking_schedule_general_function() {
-    // Your code here
+    
 }
 ?>
